e1efd582c8fd8b4c8ee82b7e80befb0c7b5f7632
Gitaristium <gitaristium@gmail.com>
2021-04-08 16:00:13 +0700

    .

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
 
 return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+
+
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=save_coin',
+    'username' => 'savecoin_user',
+    'password' => 'savecoin_pass',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
